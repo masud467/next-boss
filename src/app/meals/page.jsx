5ee46c6531cee4fe74224b01be3b13b0c1f1eea2
masud467 <masudpfrnt@gmail.com>
2024-11-30 12:35:42 +0600

    added CSS Modules file for styling on meals route.

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,13 +1,13 @@
-
 import Meals from "@/components/Meals";
 import React from "react";
+import styles from "./styles.module.css";
 
 const Page = () => {
   return (
     <div className="p-12">
       <h1 className="text-3xl font-semibold text-red-400">Choose your meals</h1>
-      <p className="text-xl font-medium">Find your meals by searching...</p>
-      <div >
+      <p className={styles.font_tomato}>Find your meals by searching...</p>
+      <div>
         <Meals></Meals>
       </div>
     </div>
