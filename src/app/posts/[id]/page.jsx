5ecd2364f128ff173e93f03723cf851433d8d1f9
masud467 <masudpfrnt@gmail.com>
2024-12-10 16:49:40 +0600

    update metadata on view details dynamic page.

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React from "react";
 const getPostsDetails = async (id) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-  const data = await res.json()
-  return data
-}
-const Page = async ({params}) => {
-  const {title,body} = await getPostsDetails(params.id)
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await res.json();
+  return data;
+};
+export const generateMetadata = async ({ params }) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
+  const postData = await res.json();
+  return {
+    title: {
+      absolute: `${postData.title}`,
+    },
+    description: postData.body,
+    keywords: postData.body.split(" "),
+  };
+};
+const Page = async ({ params }) => {
+  const { title, body } = await getPostsDetails(params.id);
   return (
     <div>
-      <h4>
-        Title: {title}
-      </h4>
-      <h4>
-        Description: {body}
-      </h4>
+      <h4>Title: {title}</h4>
+      <h4>Description: {body}</h4>
     </div>
   );
-}
+};
 
 export default Page;
