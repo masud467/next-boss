7d75f49257452531a35d3e712d87c6e43591d6f5
masud467 <masudpfrnt@gmail.com>
2024-12-10 16:48:23 +0600

    update metadata on about page.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,12 @@
 import AboutContent from "@/components/AboutContent";
 import React from "react";
 
+export const metadata = {
+  title: "About",
+  description: "About Page",
+  keywords: ["about", "about page"],
+};
+
 const Page = () => {
   return (
     <div>
