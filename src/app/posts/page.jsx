b2c0dcf80b62bcc9c9ac8cf8562a4e33a42953a2
masud467 <masudpfrnt@gmail.com>
2024-12-11 16:49:45 +0600

    update code formating.

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -10,11 +10,8 @@ const Page = async () => {
     <div className="pb-5 px-5">
       <h4>All Posts</h4>
       <div className="grid grid-cols-4 gap-6">
-        {postsData?.slice(0, 20)?.map(({title, body, id}) => (
-          <div
-            key={id}
-            className="border-2 p-6 flex flex-col justify-between"
-          >
+        {postsData?.slice(0, 20)?.map(({ title, body, id }) => (
+          <div key={id} className="border-2 p-6 flex flex-col justify-between">
             <h4>
               <span className="font-bold">Title:</span> {title}
             </h4>
@@ -22,7 +19,7 @@ const Page = async () => {
               <span className="font-bold">Description:</span> {body}
             </p>
             <button className="bg-[#B91C1C] px-4 py-2 rounded-md text-white">
-              <Link href={`/posts/${id}`}>see details</Link> 
+              <Link href={`/posts/${id}`}>see details</Link>
             </button>
           </div>
         ))}
