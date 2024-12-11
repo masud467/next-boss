7914b66c67d04da7fa16f9375106631646287809
masud467 <masudpfrnt@gmail.com>
2024-12-11 14:04:11 +0600

    update(about page):added Caching and Revalidating.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,10 +6,22 @@ export const metadata = {
   description: "About Page",
   keywords: ["about", "about page"],
 };
+const getTime = async () => {
+  const res = await fetch("http://localhost:3000/time", {
+    next: {
+      revalidate: 10,
+    },
+  });
+  const data = await res.json();
+  return data.currentTime;
+};
 
 const Page = () => {
+  const currentTime = getTime();
   return (
     <div>
+      <h1>About page</h1>
+      <p>Time: {currentTime}</p>
       <AboutContent></AboutContent>
     </div>
   );
