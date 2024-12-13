75afb58a94c92036d47b777bd0e76ef4f38960de
masud467 <masudpfrnt@gmail.com>
2024-12-13 08:16:19 +0600

    add user api with get and post method.

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,5 +1,38 @@
 export async function GET() {
-  return Response.json({
-    message: "Hello users",
+  return Response.json(users, {
+    headers: {
+      "Set-Cookie": "theme=dark",
+    },
   });
 }
+
+export async function POST(request) {
+  const newUser = await request.json();
+  users.push({
+    id: users.length + 1,
+    name: newUser.name,
+    email: newUser.email,
+  });
+  return Response.json({
+    message: "New user added",
+    users,
+  });
+}
+
+const users = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john@example.com",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    email: "jane@example.com",
+  },
+  {
+    id: 3,
+    name: "John Smith",
+    email: "johnsmith@example.com",
+  },
+];
