6f1b4fe9e1d5a344ea26591279e9c8dc0c3baaab
masud467 <masudpfrnt@gmail.com>
2024-12-20 12:57:30 +0600

    added new website hostname in next config file.

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,21 @@
 const nextConfig = {
   images: {
     remotePatterns: [
+      // {
+      //   protocol: "https",
+      //   hostname: "*",
+      //   port: "",
+      //   pathname: "/**",
+      // },
       {
         protocol: "https",
         hostname: "www.themealdb.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
         port: "",
         pathname: "/**",
       },
