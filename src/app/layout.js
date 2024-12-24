f27f8ac2c02e4074d54c36c42123a74fbe6f8b2c
masud467 <masudpfrnt@gmail.com>
2024-12-24 22:50:07 +0600

    added authprovider in layout file.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import AuthProvider from "./services/AuthProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,14 +26,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <NavBar></NavBar>
+      <AuthProvider>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <NavBar></NavBar>
 
-        <div>{children}</div>
-        <Footer></Footer>
-      </body>
+          <div>{children}</div>
+          <Footer></Footer>
+        </body>
+      </AuthProvider>
     </html>
   );
 }
